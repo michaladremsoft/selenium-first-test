a7a6b54f053b1c8446039d85ac48725f89326747
Michał Jaglarz <michal.jaglarz@ad.adrem>
2026-05-08 13:37:00 +0200

    Add negative login test

---
+++ src/test/java/NegativeLoginTest.java
@@ -0,0 +1,27 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NegativeLoginTest {
+
+    @Test
+    public void invalidLoginShowsError() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("wrong_password");
+        driver.findElement(By.id("login-button")).click();
+
+        String errorMessage = driver.findElement(By.cssSelector("[data-test='error']")).getText();
+
+        Assert.assertTrue(errorMessage.contains("Username and password do not match"));
+
+        driver.quit();
+    }
+}
